f6ecc453d490bcfecd6feaf4122db276743ce2d9
Keren <keren.ariellemr@gmail.com>
2023-06-25 19:02:27 -0300

    Fix headers

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,6 +2,9 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
+  headers: {
+    'Origin': 'https://ramos-automoveis-fe-47a51f7ed71c.herokuapp.com'
+  }
 });
 
 export default api;
