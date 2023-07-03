5f3da03c1f2d4801128f4dae74701fc855e49069
Keren <keren.ariellemr@gmail.com>
2023-07-03 09:37:50 -0300

    ORIGIN

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,6 +2,9 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
+  headers: {
+    'Origin': process.env.REACT_APP_FRONTEND
+  }
 });
 
 export default api;
