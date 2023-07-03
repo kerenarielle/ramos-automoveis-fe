a290c7226254df8ce5f988bc9931a39035f56a2d
Keren <keren.ariellemr@gmail.com>
2023-07-03 17:47:56 -0300

    data

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -24,17 +24,7 @@ const Home: React.FC = () => {
     compra: [],
   });
 
-  console.log(
-    data.compra[0].dt_compra
-      ? format(
-          utcToZonedTime(
-            new Date(data.compra[0].dt_compra),
-            "America/Sao_Paulo"
-          ),
-          "dd/MM/yyyy"
-        )
-      : ""
-  );
+  console.log(data);
 
   useEffect(() => {
     async function getInfo() {
